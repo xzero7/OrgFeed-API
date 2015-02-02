b82c54137c6f83b4535fee2a8ac400847df6ec79
xzero7 <xzero7@gmail.com>
2015-02-02 15:35:25 -0500

    created skeleton for userExists function

--- include/sqlGenerator.php
+++ include/sqlGenerator.php
@@ -6,5 +6,18 @@
 @purpose: Create sql statments per DB table
 */
 
+/*
+	@purpose- will check if suggested user exist by verifying email
+	@params
+		-email - the email being checked
+		-db    - the db instance class to execute sql
+	@return
+		-true  - if user already exists
+		-false - user does not exist
+*/
+private function userExists($email, $db)
+{
+	
+}
 
 ?>
