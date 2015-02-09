c07646bbf1f6dd5e7423c354ef3f84ec4982383e
xzero7 <xzero7@gmail.com>
2015-02-09 16:14:51 -0500

    added new documentation and apigenerator function

--- include/sqlExecuter.php
+++ include/sqlExecuter.php
@@ -49,13 +49,27 @@ private function userExists($email)
 }
 
 /*
-	@purpose- will check if suggested user exist by verifying email
-	@params
-		-email - the email being checked
-		-db    - the db instance class to execute sql
+	@purpose- will generate an api key for user created using md5
+	@params 
 	@return
-		-true  - if user already exists
-		-false - user does not exist
+		string - api string for user  
+
+*/ 
+private function generateApiKey() 
+{
+    return md5(uniqid(rand(), true));
+}
+
+/*
+	@purpose- will create a new user in DB
+	@params
+		-email       - the email for new user
+		-password    - password for new user 
+		-firstName   - first name of new user
+		-lastName    - last name of new user
+	@return
+		-success  - response code for success
+		-fail     - response code for failure
 */
 public function createUser($email, $password, $firstName, $lastName)
 {
